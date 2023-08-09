d73c26ac3c667396288e766dab003245cfa82c78
Dikromo <fhr.dikromo@gmail.com>
2023-08-09 14:17:11 +0700

    memperbaiki query dashbpard sales 2 update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -194,7 +194,7 @@ class DashboardController extends Controller
             ->addIndexColumn()
             ->addColumn('today', function ($data) {
                 $vtdt = $data->total_nocall + $data->total_call_today;
-                $vToday = '<span style="color:#009b9b">' . $vtdt . '(' . $data->total_nocall - $data->total_nocall_today + $data->total_call_today . ' + ' . $data->total_call_today + $data->total_nocall_today . ')</span>';
+                $vToday = '<span style="color:#009b9b">' . $vtdt . '(' . $data->total_nocall - $data->total_nocall_today . ' + ' . $data->total_call_today + $data->total_nocall_today . ')</span>';
                 $vToday .= ' | ';
                 $vToday .= '<span style="color:#eb7904">' . $data->total_call_today . '</span>';
                 $vToday .= ' | ';
@@ -299,7 +299,7 @@ class DashboardController extends Controller
             //     <td>' . $item->total_callout_3 . '</td>
             // </tr>';
 
-            $vToday = '<span style="color:#009b9b">' . $item->total_call_today + $item->total_nocall . '(' . $item->total_nocall - $item->total_nocall_today +  $item->total_call_today . ' + ' . +$item->total_call_today + $item->total_nocall_today . ')</span>';
+            $vToday = '<span style="color:#009b9b">' . $item->total_call_today + $item->total_nocall . '(' . $item->total_nocall - $item->total_nocall_today . ' + ' . +$item->total_call_today + $item->total_nocall_today . ')</span>';
             $vToday .= ' | ';
             $vToday .= '<span style="color:#eb7904">' . $item->total_call_today . '</span>';
             $vToday .= ' | ';
